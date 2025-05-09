070e35ddbab903c12ffbf056ec1a4e00131b3ac0
Lucas <yuutasf@gmail.com>
2025-05-09 09:13:06 -0300

    app almost done
    img's import
    update color palette from variables.scss
    index.tsx update with more components, links to social media, etc.

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -3,35 +3,41 @@ import "./styles.scss";
 
 const Container = () => {
 	return (
-		<div>
-			<h1>LVCXS</h1>
-			<h2>On the path to modernization</h2>
-			<img src="#" alt="Profile Photo" />
-			<ul>
-				<li>
+		<div className="container">
+			<h1>Hello, I'm lvcxs</h1>
+			<h2 id="subtitle">On the path to modernization</h2>
+			<img className="profile-photo" src="./user.png" alt="Profile Photo" />
+			<ul className="link-wrapper">
+				<li className="social-list">
 					<Link
+						id="linkedin"
+						className="btn-social"
 						to="https://www.linkedin.com/in/dev-lucastorres/"
 						target="_blank"
 						aria-label="Linkedin Profile"
 					></Link>
 				</li>
-				<li>
+				<li className="social-list">
 					<Link
+						id="github"
+						className="btn-social"
 						to="https://github.com/lucas-torres-dev/"
 						target="_blank"
 						aria-label="Github Profile"
 					></Link>
 				</li>
-				<li>
+				<li className="social-list">
 					<Link
+						id="x"
+						className="btn-social"
 						to="https://x.com/l_v_c_a_x/"
 						target="_blank"
 						aria-label="x.com Profile"
 					></Link>
 				</li>
 			</ul>
-			<h3>My Projects</h3>
-			<div>
+			<h3 id="cards-header">My Projects</h3>
+			<div className="cards">
 				<div>
 					<h4>Project One</h4>
 					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
